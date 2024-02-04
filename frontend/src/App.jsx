86a5f4f531d4c4407b40b72e852c07cab164ccba
kom-senapati <komnoob123@gmail.com>
2024-02-04 11:46:20 +0530

    code: Added Landing Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,10 @@
-import React, { useEffect, useState } from "react";
 import "./App.css";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    fetch("http://localhost:3001/")
-      .then((res) => res.text())
-      .then((data) => setData(data))
-      .catch((err) => console.log(err.message));
-  }, []);
-
   return (
-    <div className="text-black p-10 font-mono text-center">
-      <header>
-        <h1 className="text-2xl">Data from Express:</h1>
-        {data ? <p>{data}</p> : <p>Loading ...</p>}
-      </header>
+    <div>
+      <LandingPage />
     </div>
   );
 }
