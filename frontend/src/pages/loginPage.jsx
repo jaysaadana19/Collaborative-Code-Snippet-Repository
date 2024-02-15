b638eb1ac42498f0c123ba48f1043b42dae05b9f
Sambit <110420002+sambitsingha@users.noreply.github.com>
2024-02-15 21:02:56 +0530

    Refactor loginPage component styling

--- frontend/src/pages/loginPage.jsx
+++ frontend/src/pages/loginPage.jsx
@@ -4,16 +4,18 @@ import github from "../assets/github.png";
 const Login = () => {
 	return (
 		<>
-			<div className='wrapper bg-login-bg w-screen bg-center bg-cover bg-no-repeat flex justify-around items-center px-5 flex-wrap h-screen'>
-				<div className='text-white  bg-transparent backdrop-blur p-5 rounded-3xl'>
-					<h1 className='text-7xl my-3 italic'>CodeCollab</h1>
-					<h1 className='text-4xl underline italic'>
+			<div className='wrapper bg-login-bg bg-center bg-cover bg-no-repeat flex justify-around items-center px-5 h-full md:flex-row flex-col'>
+				<div className='text-white  bg-transparent backdrop-blur p-5 rounded-3xl text'>
+					<h1 className='text-4xl sm:text-5xl md:text-7xl my-3 italic'>
+						CodeCollab
+					</h1>
+					<h1 className='text-2xl sm:text-3xl md:text-4xl underline italic'>
 						# Collaborative Code Snippets
 					</h1>
 				</div>
-				<div className='flex justify-end items-center sm:h-screen mx-20'>
-					<div className='w-96 bg-white/10 p-6 rounded-[40px] shadow-lg backdrop-blur h-auto flex flex-col justify-center'>
-						<h1 className='text-2xl font-bold text-center mb-8 text-white underline'>
+				<div className='flex justify-end items-center sm:h-screen mx-20 w-1/3 login'>
+					<div className='w-72 md:w-[450px] bg-white/10 p-6 rounded-[40px] shadow-lg backdrop-blur h-auto flex flex-col justify-center'>
+						<h1 className='text-3xl font-bold text-center mb-8 text-white underline'>
 							Join Our Community
 						</h1>
 						<form action=''>
@@ -46,25 +48,25 @@ const Login = () => {
 								/>
 							</div>
 							<div className='flex justify-center items-center'>
-								<button className='bg-blue-500 text-white px-5 py-2 rounded-full font-medium w-24 hover:bg-blue-600 hover:duration-300'>
+								<button className='bg-blue-500 text-white px-5 py-2 rounded-full font-medium w-24 hover:bg-blue-800 hover:duration-300'>
 									Login
 								</button>
 							</div>
 							<h1 className='text-center text-white my-5'>
 								Don't have an account ?{" "}
 								<a href='#'>
-									<span className='text-emerald-400 hover:text-emerald-200 hover:duration-300'>
+									<span className='text-emerald-400 hover:text-red-600 hover:font-bold hover:duration-300'>
 										Sign Up
 									</span>
 								</a>{" "}
 								here
 							</h1>
 							<div className='buttons flex flex-col justify-center items-center'>
-								<button className='bg-white flex justify-center items-center rounded-full w-60 py-1'>
+								<button className='bg-white flex justify-center items-center rounded-full w-60 py-1 duration-300 ease-linear hover:bg-slate-800 hover:text-white'>
 									<img src={google} alt='' className='w-7' />
 									Sign in with Google
 								</button>
-								<button className='bg-white flex justify-center items-center rounded-full  h-10 my-5 py-1 w-60'>
+								<button className='bg-white flex justify-center items-center rounded-full  h-10 my-5 py-1 w-60 hover:bg-slate-800 hover:text-white duration-300 ease-linear'>
 									<img src={github} alt='' className='w-10' />
 									Sign in with Github
 								</button>
